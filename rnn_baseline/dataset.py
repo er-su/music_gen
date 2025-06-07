830d875c1ffc1feeaa90a0c82756c4e651265bf1
er-su <erick.sun07@gmail.com>
2025-06-07 16:03:26 -0700

    Addition of dataset obj

---
+++ rnn_baseline/dataset.py
@@ -0,0 +1,113 @@
+import sys
+import random
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from typing import Union, Tuple, List
+from torch.utils.data import Dataset, DataLoader
+
+sys.path.append("../")
+
+from preprocess import Preprocessor, output_type_literal
+
+def collate(combined: Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray]]) -> Tuple[np.ndarray[np.ndarray], np.ndarray[np.ndarray], np.ndarray[np.ndarray]]:
+    datas, labels, arrays = combined
+
+
+class MidiDataset(Dataset):
+    '''
+    A wrapper class for the preprocessor for use with Pytorch's built in dataset object.
+    If a dataframe path is provided, all other inputs except slice are ignored and the dataframe is loaded and parsed
+    '''
+    def __init__(
+        self, 
+        dataframe_path: Union[Path, str] = None,
+        sliced: Union[int, None] = None,
+        surname: str = None,
+        folder_path:Union[Path, str] = Path("surname_checked_midis"),
+        get_dict:bool=True,
+        binarize:bool=True,
+        lookback:int=1,
+        resolution:int=8,
+        output_type:output_type_literal = "chordify_string"
+    ):
+        self.dataframe_path = dataframe_path if type(dataframe_path) == Path else Path(dataframe_path)
+        self.sliced = sliced
+
+        if self.dataframe_path == None:
+            self.preprocessor = Preprocessor(
+                folder_path=folder_path,
+                get_dict=get_dict,
+                binarize=binarize,
+                lookback=lookback,
+                resolution=resolution,
+                output_type=output_type
+            )
+
+            self.preprocessor.collect(surname=surname)
+
+        else:
+            self.data, self.labels, self.durations = self.df_to_array(pd.read_csv(dataframe_path))
+
+    def df_to_array(self, dataframe: pd.DataFrame) -> Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
+        '''
+        Converts the loaded dataframe into three separate numpy arrays holding values, labels and durations.
+        The dataframe is assumed to have a lookback of 1.
+        '''
+        data = []
+        labels = []
+        durations = []
+        
+        numpy_form = dataframe.to_numpy()
+        for i, row in enumerate(numpy_form):
+            if row[-1] == 0:
+                if i != 0:
+                    data.append(np.array(running_data))
+                    labels.append(np.array(running_labels))
+                    durations.append(np.array(running_durations))
+
+                running_data = []
+                running_labels = []
+                running_durations = []
+                
+            running_data.append(row[0])
+            running_labels.append(row[1])
+            running_durations.append(row[2])
+
+        return (data), (labels), (durations)
+
+    def __len__(self) -> int:
+        if self.dataframe_path == None:
+            return self.preprocessor.__len__()
+        else:
+            return len(self.data)
+    
+    def __getitem__(self, index) -> Tuple[np.ndarray, Union[np.ndarray, None], Union[np.ndarray, None]]:
+        
+        if self.dataframe_path == None:
+            data, labels, durations = super().__getitem__(index)
+            if self.sliced == None:
+                return data, labels, durations
+            
+            if not (len(data) >= self.sliced):
+                raise ValueError("Slice length is longer than song")
+            
+            randint = random.randint(0, len(data) - self.sliced)
+            return data[randint: randint + self.sliced], labels[randint: randint + self.sliced], durations[randint: randint + self.sliced]
+        
+        else:
+            data, labels, durations = self.data[index], self.labels[index], self.durations[index]
+            if self.sliced == None:
+                return data, labels, durations
+            
+            if not (len(data) >= self.sliced):
+                raise ValueError("Slice length is longer than song")
+            
+            randint = random.randint(0, len(data) - self.sliced)
+            return data[randint: randint + self.sliced], data[randint: randint + self.sliced], data[randint: randint + self.sliced]
+
+if __name__ == "__main__":
+    dataset = MidiDataset(dataframe_path="../output/Agnew_chordify_int_data.csv", sliced=256)
+    dataloader = DataLoader(dataset, batch_size = 2, shuffle=True)
+    for data, label, duration in dataloader:
+        print(f"Batch shape: {data.shape}, Labels: {label.shape}")
