ee61521e5c1f6f93b59ca7f258c28b13e485a06b
er-su <erick.sun07@gmail.com>
2025-06-11 04:31:18 -0700

    Update mlp architecture

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -18,32 +18,24 @@ class MusicMLP(nn.Module):
         )
 
         self.duration_model = nn.Sequential(
-            #CAHNGED
-            #nn.Linear(in_features=12 + 1, out_features=256),
             nn.Linear(in_features=12, out_features=64),
             nn.GELU(),
             nn.Dropout(0.5),
-            nn.Linear(in_features=64, out_features=128),
-            nn.GELU(),
-            nn.Dropout(0.5),
-            nn.Linear(in_features=128, out_features=1),
+            nn.Linear(in_features=64, out_features=1),
             nn.Softplus()
         )
         
 
-    def forward(self, data, durations):
+    def forward(self, data):
         # Data is of shape seq_len, 12 and should have a set of zeros as its starting point appended
         data_preds = self.note_model(data)
-        #CHANGED
-        #duration_preds = self.duration_model(torch.cat((data, durations.unsqueeze(axis=-1)), -1))
         duration_preds = self.duration_model(data)
 
         return data_preds, duration_preds
     
     def infer(self, z, seq_len):
         # Z should be a 12 dim vector
-        alpha = 0.75
-        temp = 2
+        alpha = 0.30
         input = z
         notes = torch.zeros((seq_len, 12))
         durations = torch.zeros((seq_len, 1))
@@ -57,32 +49,9 @@ class MusicMLP(nn.Module):
 
             note_pred = torch.sigmoid(note_pred)
 
-            # New stuff
-            """ total_mass = note_pred.sum().item()
-            p_dyn = total_mass * alpha
-            sorted_probs, sorted_idx = torch.sort(note_pred, descending=True)
-            cumsum = 0.0
-            keep_idx = []
-
-            for i,v in enumerate(sorted_probs):
-                cumsum += v.item()
-                keep_idx.append(sorted_idx[i].item())
-
-                if cumsum >= p_dyn and len(keep_idx) >= 1:
-                    break
-
-            mask = torch.zeros_like(note_pred)
-            mask[keep_idx] = 1.0
-            filtered = note_pred * mask
-            sampled = torch.bernoulli(filtered)
-            note_pred = sampled """
-
-            # new stuff again
-            """ topk = torch.topk(note_pred, 5)
-            mask = torch.zeros_like(note_pred)
-            mask.scatter_(0, topk.indices, 1.0) """
-            #newer stuff
+            
             total_mass = note_pred.sum().item()
+            print(note_pred.shape)
             p_dyn = total_mass * alpha
             topk = torch.topk(note_pred, 12)
             topk_vals = topk.values
@@ -98,7 +67,7 @@ class MusicMLP(nn.Module):
                     break
 
             mask = torch.ones_like(note_pred)
-            mask = mask * 0.75
+            mask = mask * 0.80
             mask[valid_inds] = 1.0 
 
             
